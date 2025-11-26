2a512dff53a84fb6de21fa1c1b460487a13b19b5
Nicola Kaleta <nic00la.kaleta@gmail.com>
2025-11-26 10:18:08 +0100

    Dodaj pliki projektów.

---
+++ WzornikKolorow/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WzornikKolorow
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WzornikKolorow/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WzornikKolorow")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WzornikKolorow.Pages")]
