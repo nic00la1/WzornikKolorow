e0a74b357911fc5c02a411c4aa802942e2203105
Nicola Kaleta <nic00la.kaleta@gmail.com>
2025-11-26 10:54:28 +0100

    oddzialowanie slidera na label

--- WzornikKolorow/MainPage.xaml.cs
+++ WzornikKolorow/MainPage.xaml.cs
@@ -6,5 +6,26 @@
         {
             InitializeComponent();
         }
+
+        private void Slider_ValueChanged(System.Object sender, Microsoft.Maui.Controls.ValueChangedEventArgs e)
+        {
+            Slider slider = sender as Slider;
+            int newValue = (int)slider.Value;
+            lblSliderRValue.Text = newValue.ToString("");
+        }
+
+        private void Slider_ValueChanged_1(System.Object sender, Microsoft.Maui.Controls.ValueChangedEventArgs e)
+        {
+            Slider slider = sender as Slider;
+            int newValue = (int)slider.Value;
+            lblSliderGValue.Text = newValue.ToString("");
+        }
+
+        private void Slider_ValueChanged_2(System.Object sender, Microsoft.Maui.Controls.ValueChangedEventArgs e)
+        {
+            Slider slider = sender as Slider;
+            int newValue = (int)slider.Value;
+            lblSliderBValue.Text = newValue.ToString("");
+        }
     }
 }
